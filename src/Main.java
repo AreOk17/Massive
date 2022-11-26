6bc29f629782152c55a5555d5c55d409a84c1633
1 <Anasko162@Mail.ru>
2022-11-26 18:12:36 +0300

    2 задание  на Массивы.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,32 @@ public class Main {
         int [] arr = new int[3];
         arr[0] = 1;
         arr[1] = 2;
-        arr[3] = 3;
+        arr[2] = 3;
         float[] array = {1.57f, 7.654f, 9.986f};
         boolean[] array2 = {true, false};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != arr.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
 
+for (int i = 0; i < array.length; i++) {
+        System.out.print(array[i]);
+        if (i != array.length - 1) {
+        System.out.print(",");
+        }
+        }
+        System.out.println();
+
+
+        for (int i = 0; i < array2.length; i++) {
+        System.out.print(array2[i]);
+        if (i != array2.length - 1) {
+        System.out.print(",");
+        }
+        }
+        System.out.println();
     }
 }
