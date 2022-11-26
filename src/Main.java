54b1f99ddc9003fc3faba6982bb1892011f5574e
1 <Anasko162@Mail.ru>
2022-11-26 18:44:41 +0300

    4 задание  на Массивы.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
+
 public class Main {
     public static void main(String[] args) {
-        int [] arr = new int[3];
+        int[] arr = new int[3];
         arr[0] = 1;
         arr[1] = 2;
         arr[2] = 3;
@@ -14,20 +15,20 @@ public class Main {
         }
         System.out.println();
 
-for (int i = 0; i < array.length; i++) {
-        System.out.print(array[i]);
-        if (i != array.length - 1) {
-        System.out.print(",");
-        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i != array.length - 1) {
+                System.out.print(",");
+            }
         }
         System.out.println();
 
 
         for (int i = 0; i < array2.length; i++) {
-        System.out.print(array2[i]);
-        if (i != array2.length - 1) {
-        System.out.print(",");
-        }
+            System.out.print(array2[i]);
+            if (i != array2.length - 1) {
+                System.out.print(",");
+            }
         }
         System.out.println();
         for (int i = arr.length - 1; i >= 0; i--) {
@@ -50,6 +51,19 @@ for (int i = 0; i < array.length; i++) {
                 System.out.print(",");
             }
         }
+            System.out.println();
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 == 1) {
+                    arr[i]++;
+                }
+
+            System.out.print(arr[i]);
+            if (i != arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
         System.out.println();
     }
-}
+
+    }
