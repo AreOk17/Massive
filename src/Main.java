edfb3ee28651c0b27ff95da19dffc5476fb5d10b
1 <Anasko162@Mail.ru>
2022-11-26 17:47:26 +0300

    1 задание  на Массивы.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[3] = 3;
+        float[] array = {1.57f, 7.654f, 9.986f};
+        boolean[] array2 = {true, false};
+
+    }
+}
